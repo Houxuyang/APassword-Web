47b1fde1091fe759a40b2652d7ecdf4c16d46bbe
Aegis <onlyforanonymous@163.com>
2017-02-11 23:17:59 +0800

    ES 1.0, function is available.
    Signed-off-by: Aegis <onlyforanonymous@163.com>

---
+++ data.js
@@ -0,0 +1,4 @@
+var data=[["BALKjmD4hlh3jw==","BQKXkGD4hlgIT0aP/g==","BQIRCWD4hli5MmKgpQ==","BQJrt2D4hliC/umLcDRN0K/PN4Q="],
+["TgE07/XTgVhlQpI=","pANe55bbgVgH+AcKHQ==","9gJIHl5ahVhwtm5S4g==","bAFM83EGh1iT5S+HOhFkbxo="],
+["2wIGDDnSgVhwyd7Izbs=","ewBcXkIpn1iN1WhPSXOmh/w=","fACaxEIpn1jAHxFn7AMowQ==","fgCefEIpn1jXeBDGCJsY1w=="],
+["awGvpHEGh1h9Jiw9yw==","bAFrtXEGh1iQ3+AS","bAGY0XEGh1j+BgnfT9vi","bAFM83EGh1iT5S+HOhFkbxo="]]
